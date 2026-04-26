6c4a64716c4ff91f98c354ee6390d05c08aa98bf
DhanushG78 <dhanu.g783@gmail.com>
2026-04-26 23:22:28 +0530

    Add warning for missing environment variables & explicit US region

--- lib/contentstack.ts
+++ lib/contentstack.ts
@@ -1,7 +1,12 @@
 import Contentstack from 'contentstack';
 
+if (!process.env.NEXT_PUBLIC_API_KEY || !process.env.NEXT_PUBLIC_DELIVERY_TOKEN || !process.env.NEXT_PUBLIC_ENVIRONMENT) {
+  console.warn("⚠️ CONTENTSTACK ENVIRONMENT VARIABLES ARE MISSING! The build might fail or generate empty pages.");
+}
+
 export const Stack = Contentstack.Stack({
   api_key: process.env.NEXT_PUBLIC_API_KEY as string,
   delivery_token: process.env.NEXT_PUBLIC_DELIVERY_TOKEN as string,
   environment: process.env.NEXT_PUBLIC_ENVIRONMENT as string,
+  region: Contentstack.Region.US,
 });
